c691880f99c978c03fdb836e7901bcd371892ac9
Unknown <daichizaki@yahoo.co.jp>
2019-08-29 02:48:05 +0900

    initial commit

---
+++ Assets/Editor/ConvertToScriptableObject.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class ConvertToScriptableObject : EditorWindow {
+
+    /**
+     * https://heasarc.gsfc.nasa.gov/cgi-bin/W3Browse/w3query.pl
+     * ヒッパルコス星座表の出力データをスクリプタブルオブジェクトに変更するメソッド
+     */
+
+    [MenuItem("PlanetaliumTools/ConvertToScriptableObject")]
+    static void Open() {
+
+        StarDataAssets starDataAsset = CreateInstance<StarDataAssets>();
+
+        string path = EditorUtility.OpenFilePanel("星座データを選んでね", "", "");
+        if (path.Length != 0) {
+            string[] lines = File.ReadAllLines(path);
+
+            int cnt = 0;
+
+            foreach (string strLine in lines) {
+
+                cnt++;
+
+                if (cnt > 5) {
+
+                    string[] strLineArr = strLine.Split('|');
+
+                    string[] raArray = strLineArr[2].Split(' ');
+                        
+                    string[] digArray = strLineArr[3].Split(' ');
+
+                    char spectType = strLineArr[4].Substring(0, 1).ToCharArray()[0];
+
+                    Color spectTypeColor = new Color();
+                        
+                    switch (spectType) {
+                        case 'O':
+                            spectTypeColor = new Color(155f, 176f, 255f) / 255f;
+                            break;
+                        case 'B':
+                            spectTypeColor = new Color(170f, 191f, 255f) / 255f;
+                            break;
+                        case 'A':
+                            spectTypeColor = new Color(202f, 215f, 255f) / 255f;
+                            break;
+                        case 'F':
+                            spectTypeColor = new Color(248f, 247f, 255f) / 255f;
+                            break;
+                        case 'G':
+                            spectTypeColor = new Color(255f, 244f, 234f) / 255f;
+                            break;
+                        case 'K':
+                            spectTypeColor = new Color(255f, 210f, 161f) / 255f;
+                            break;
+                        case 'M':
+                            spectTypeColor = new Color(255f, 204f, 111f) / 255f;
+                            break;
+                        default:
+                            spectTypeColor = new Color(0f, 0f, 0f) / 255f;
+                            break;
+                    }
+
+                    starDataAsset.starDataList.Add(
+                        new StarDataAssets.StarData(
+                            strLineArr[1],
+                            new Vector3(float.Parse(raArray[0]), float.Parse(raArray[1]), float.Parse(raArray[2])),
+                            new Vector3(float.Parse(digArray[0]), float.Parse(digArray[1]), float.Parse(digArray[2])),
+                            spectTypeColor,
+                            float.Parse(strLineArr[5])
+                            )
+                        );
+                }
+            }
+        }
+
+        AssetDatabase.CreateAsset(starDataAsset, "Assets/Resources/StarDataAsset.asset");
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/Scripts/StarDataAssets.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarDataAssets : ScriptableObject {
+    public List<StarData> starDataList = new List<StarData>();
+
+    [System.SerializableAttribute]
+    public class StarData {
+        public string hipName;
+
+        public Vector3 ra, dec;
+
+        public Color spectTypeColor;
+
+        public float vMag;
+
+        public StarData(string _hipName, Vector3 _ra, Vector3 _dec, Color _spectTypeColor, float _vMag) {
+            hipName = _hipName;
+            ra = _ra;
+            dec = _dec;
+            spectTypeColor = _spectTypeColor;
+            vMag = _vMag;
+        }
+    }
+}
